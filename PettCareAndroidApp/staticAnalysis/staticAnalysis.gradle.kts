90cc86e751a46e5146be066e8228cf5098a0b50e
lukicante <alukic@tripadvisor.com>
2024-03-04 20:50:19 +0100

    Add project and dependencies

---
+++ PettCareAndroidApp/staticAnalysis/staticAnalysis.gradle.kts
@@ -0,0 +1,12 @@
+apply(from= "$rootDir/staticAnalysis/lint/lint.gradle")
+apply(from= "$rootDir/staticAnalysis/detekt/detekt.gradle")
+apply(from= "$rootDir/staticAnalysis/ktlint/ktlint.gradle")
+
+val staticAnalysis by tasks.registering {
+    group = "verification"
+    description = "Generate StaticAnalysis reports."
+
+    dependsOn(tasks.named("ktlint"))
+    dependsOn(tasks.named("lint"))
+    dependsOn(tasks.named("detekt"))
+}
